d070fc461f483e382a560a2ee02ff71db248bab5
Ali Raza <alirazaharaj74@gmail.com>
2026-06-07 05:58:27 +0500

    Create hand_writing.py

---
+++ 06_Text_To_Handwriting/hand_writing.py
@@ -0,0 +1,6 @@
+import pywhatkit
+
+text='''Here's an explanation of the content for your presentation on "Classification and Trust Modelling: Risk Management"  '''
+
+pywhatkit.text_to_handwriting(text,'demo.png','black')          # Pahla text jesa change karna ha 2.file name jaha text save karna ha 3.color jis ma text ho
+print('Thanks.............')
